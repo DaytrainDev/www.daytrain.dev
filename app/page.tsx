dc0ceab6b6b183ef79e10600b64ec9e6e522e96c
the0bob <hob.daytrain@protonmail.com>
2024-04-12 00:34:51 -0500

    destyle

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const MapItem = (item: any, index: number) => {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative flex flex-col place-items-center">
         <h1 className="text-4xl font-bold text-center lg:text-6xl">
           Daytrain.dev
         </h1>
@@ -54,23 +54,45 @@ export default function Home() {
         <div className="flex flex-row items-center justify-center">
           {[
             {
+              title: "Crit-Fumble",
+              description: "Pretend with dice.",
+              href: "https://www.crit-fumble.com",
+              target: "_blank",
+            },
+            {
               title: "GitHub",
               description: "See code.",
               href: "https://github.com/DaytrainDev",
               target: "_blank",
             },
+          ].map(MapItem)}
+        </div>
+        <div className="flex flex-row items-center justify-center">
+          {[
             {
               title: "Discord",
               description: "Say hi.",
               href: "https://discord.gg/jmUjBskedy",
               target: "_blank",
             },
+            // {
+            //   title: "Twitch",
+            //   description: "Watch stream.",
+            //   href: "https://www.twitch.tv/daytraindev",
+            //   target: "_blank",
+            // },
             {
               title: "Patreon",
               description: "Give money.",
               href: "https://www.patreon.com/DaytrainDev",
               target: "_blank",
             },
+            // {
+            //   title: "Keyhole Software",
+            //   description: "Hire devs.",
+            //   href: "https://https://keyholesoftware.com/services",
+            //   target: "_blank",
+            // },
           ].map(MapItem)}
         </div>
       </div>
