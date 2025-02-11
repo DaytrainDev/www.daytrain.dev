7c0dbd3bc3fc97947165ad078b0a065aea311b46
Hob Daytrain <hob.daytrain@protonmail.com>
2025-02-11 13:29:28 -0600

    fix api endpoints for build

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -2,13 +2,11 @@ import { search as geoSearch } from "@/lib/services/geocode";
 
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { 
-  params: { searchText } } : {params: { searchText: string }
-}) {
-  if (!searchText) {
+export async function GET(request: Request, { params } : {params: { searchText?: string }}) {
+  if (!params?.searchText) {
     return NextResponse.json({ message: 'Please include a value for `searchText`.'});
   }
   
   // TODO: return location data when user is authenticated
-  return NextResponse.json(await geoSearch(searchText));
+  return NextResponse.json(await geoSearch(params?.searchText));
 }
--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -3,13 +3,11 @@ import { search as weatherSearch} from "@/lib/services/weather";
 
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { 
-  params: { searchText } } : {params: { searchText: string }
-}) {
-  if (!searchText) {
+export async function GET(request: Request, { params } : {params: { searchText?: string }}) {
+  if (!params?.searchText) {
     return NextResponse.json({ message: 'Please include a value for `searchText`.'});
   }
   
-  // TODO:NextResponse return location data when user is authenticated
-  return NextResponse.json(await weatherSearch(await geoSearch(searchText)));
+  // TODO: return location data when user is authenticated
+  return NextResponse.json(await weatherSearch(await geoSearch(params?.searchText)));
 }
