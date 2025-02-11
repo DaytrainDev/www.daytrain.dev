282deb014d0783f081fb2ed43f28dec9a8803509
Hob Daytrain <hob.daytrain@protonmail.com>
2025-02-11 16:45:46 +0000

    add storybook

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -5,6 +5,10 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request, { 
   params: { searchText } } : {params: { searchText: string }
 }) {
+  if (!searchText) {
+    return NextResponse.json({ message: 'Please include a value for `searchText`.'});
+  }
+  
   // TODO: return location data when user is authenticated
   return NextResponse.json(await geoSearch(searchText));
 }
